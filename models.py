59f8759369c3d048e4c4a5a0e3a41ebe60af883f
AleksSM <aleks-m83@mail.ru>
2024-03-18 19:01:47 +0400

    add config.py and change manage.py

--- models.py
+++ models.py
@@ -2,15 +2,15 @@ import os
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncAttrs
 from sqlalchemy.orm import DeclarativeBase, mapped_column, Mapped
 from sqlalchemy import JSON
+from config import POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DB, POSTGRES_PORT, POSTGRES_HOST, TYPE_DB
 
+# POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD', 'secret')
+# POSTGRES_USER = os.getenv('POSTGRES_USER', 'swapi')
+# POSTGRES_DB = os.getenv('POSTGRES_DB', 'swapi')
+# POSTGRES_HOST = os.getenv('POSTGRES_HOST', 'localhost')
+# POSTGRES_PORT = os.getenv('POSTGRES_PORT', '5431')
 
-POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD', 'secret')
-POSTGRES_USER = os.getenv('POSTGRES_USER', 'swapi')
-POSTGRES_DB = os.getenv('POSTGRES_DB', 'swapi')
-POSTGRES_HOST = os.getenv('POSTGRES_HOST', 'localhost')
-POSTGRES_PORT = os.getenv('POSTGRES_PORT', '5431')
-
-PG_DSN = f'postgresql+asyncpg://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}'
+PG_DSN = f'{TYPE_DB}+asyncpg://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}'
 
 engine = create_async_engine(PG_DSN)
 
