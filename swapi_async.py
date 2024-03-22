0cdc543669c37cbb0c351aaca7628b5d6d5d46b3
AleksSM <aleks-m83@mail.ru>
2024-03-22 20:03:54 +0400

    change get data from swapi

--- swapi_async.py
+++ swapi_async.py
@@ -22,11 +22,12 @@ async def get_data(client, url_list, key):
         http_response = await client.get(url)
         json_result = await http_response.json()
         list_data.append(json_result.get(key))
-    return list_data
+    result = ', '.join(list_data)
+    return result
 
 
 async def insert_to_db(list_of_json):
-    models = [SwapiPeople(json=json_item) for json_item in list_of_json]
+    models = [SwapiPeople(**data) for data in list_of_json]
     async with Session() as session:
         session.add_all(models)
         await session.commit()
@@ -43,24 +44,21 @@ async def main():
             result[i].pop('edited', 'Key not found')
             result[i].pop('created', 'Key not found')
             result[i].pop('url', 'Key not found')
-            if 'vehicles' in result[i]:
-                vehicles = await get_data(client, result[i].get('vehicles'), 'name')
-            if 'starships' in result[i]:
-                starships = await get_data(client, result[i].get('starships'), 'name')
-            if 'films' in result[i]:
-                films = await get_data(client, result[i].get('films'), 'title')
-            if 'species' in result[i]:
-                species = await get_data(client, result[i].get('species'), 'name')
-            if 'homeworld' in result[i]:
-                homeworld = await get_data(client, result[i].get('homeworld'), 'name')
 
-            print('name_person: ', person_name)
-            print('homeworld: ', *homeworld)
-            print('films: ', ', '.join(films))
-            print('species: ', ', '.join(species))
-            print('starships ', ', '.join(starships))
-            print('vehicles : ', ', '.join(vehicles))
-            asyncio.create_task(insert_to_db(result))
+            person_name = result[i].get('name')
+            if 'vehicles' in result[i]:
+                result[i]['vehicles'] = await get_data(client, result[i].get('vehicles'), 'name')
+            if 'starships' in result[i]:
+                result[i]['starships'] = await get_data(client, result[i].get('starships'), 'name')
+            if 'films' in result[i]:
+                result[i]['films'] = await get_data(client, result[i].get('films'), 'title')
+            if 'species' in result[i]:
+                result[i]['species'] = await get_data(client, result[i].get('species'), 'name')
+            if 'homeworld' in result[i]:
+                result[i]['homeworld'] = await get_data(client, result[i].get('homeworld'), 'name')
+
+            print(result[i])
+            asyncio.create_task(insert_to_db(result[i]))
 
     tasks_set = asyncio.all_tasks() - {asyncio.current_task()}
     await asyncio.gather(*tasks_set)
